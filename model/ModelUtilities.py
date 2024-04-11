d933ea7b601bb3255092a8191b168de8a77e4300
this-dot-god <gaurxarjun@gmail.com>
2024-04-11 21:29:55 +0530

    MODIFY - model/ModelUtilities.py 1) Made a separate directory for pickles named "pickle". Now, SaveModel() and LoadModel() work with files - "pickle/Vectorizer.py" and "pickle/Model.py" 2) SaveModel() now also saves Vectorizer passed as parameter in file pickle/Vectorizer.py 3) LoadModel() now also loads Vectorizer present in file pickle/Vectorizer.py and returns both Vectorizer as well as Model 4) TrainModel() was renamed as InitModel() 5) Some comments were changed and Function definitions were reordered

--- model/ModelUtilities.py
+++ model/ModelUtilities.py
@@ -1,16 +1,40 @@
 # Import the PreprocessDatasets function from preprocessing/Preprocess.py 
 from preprocessing.Preprocess import PreprocessDatasets  
-# Import TfidfVectorizer function from sklear.feature_extraction.text module for creating a text vectorizer object
+# Import TfidfVectorizer function from sklearn.feature_extraction.text module for creating a text vectorizer object
 from sklearn.feature_extraction.text import TfidfVectorizer 
 # Import the Support Vector Machine classifier from sklearn
 from sklearn import svm  
-# Import the pickle module for saving and loading models
+# Import the pickle module for saving and loading vectorizers and models 
 import pickle  
 
-# TrainModel() creates a Linear SVC model and fits it on the datasets present at path DatasetPath 
-def TrainModel(DatasetPath):
-    # Create a Support Vector Machine classifier with a linear kernel and C=1.0
-    classifier = svm.SVC(kernel="linear", C=1.0)
+# SaveModel() makes pickle of the Vectorizer and Model passed to it
+def SaveModel(Vectorizer, Model):
+    # Open a file named "pickle/Vectorizer.pkl" in binary write mode
+    with open("pickle/Vectorizer.pkl", "wb") as file :
+        # Serialize and save the Vectorizer to the file
+        pickle.dump(Vectorizer, file)
+    # Open a file named "pickle/Model.pkl" in binary write mode
+    with open("pickle/Model.pkl", "wb") as file:
+        # Serialize and save the Model to the file
+        pickle.dump(Model, file)
+
+# LoadModel() loads the Vectorizer present in file "pickle/Vectorizer.pkl" and the Model present in file "pickle/Model.pkl" and returns them
+def LoadModel():
+    # Open the file "pickle/Vectorizer.pkl" in binary read mode
+    with open("pickle/Vectorizer.pkl", "rb") as file :
+        # Deserialize and load the vectorizer from the file
+        vectorizer = pickle.load(file)
+    # Open the file "model.pkl" in binary read mode
+    with open("pickle/Model.pkl", "rb") as file:
+        # Deserialize and load the model from the file
+        model = pickle.load(file)
+    # Return the loaded vectorizer model 
+    return vectorizer, model
+
+# InitModel() creates a TF-IDF vectorizer and a Linear SVC model and fits it on the datasets present at path DatasetPath. Further, It saves the Vectorizer and Model as well as returns them 
+def InitModel(DatasetPath):
+    # Create a Support Vector Machine model with a linear kernel and C=1.0
+    model = svm.SVC(kernel="linear", C=1.0)
     # Preprocess the dataset located at DatasetPath
     dataset = PreprocessDatasets(DatasetPath)
     # Extract the 'text' column from the dataset
@@ -21,30 +45,16 @@ def TrainModel(DatasetPath):
     vectorized_text = vectorizer.fit_transform(text)
     # Extract the 'sentiment' column from the dataset
     sentiment = dataset.pop("sentiment")
-    # Train the classifier using the vectorized text data and sentiment labels
-    classifier.fit(vectorized_text, sentiment)
+    # Train the model using the vectorized text data and sentiment labels
+    model.fit(vectorized_text, sentiment)
+    # Save the fitted Model as well as the fitted Vectorizer
+    SaveModel(vectorizer, model)
     # Return the fitted Model as well as the fitted Vectorizer
-    return vectorizer, classifier
+    return vectorizer, model
 
-# SaveModel() makes pickle of the Model passed to it
-def SaveModel(Model):
-    # Open a file named "model.pkl" in binary write mode
-    with open("model.pkl", "wb") as file:
-        # Serialize and save the model to the file
-        pickle.dump(Model, file)
-
-# LoadModel() loads the model present in file "model.pkl" and returns it
-def LoadModel():
-    # Open the file "model.pkl" in binary read mode
-    with open("model.pkl", "rb") as file:
-        # Deserialize and load the model from the file
-        model = pickle.load(file)
-    # Return the loaded model
-    return model
-
-# Predict() predicts the sentiment of String using the Model and Vectorizer passed to it
-# NOTE : Both Model and Vectorizer both should be fitted on some datasets before making a prediction
-def Predict(Model, Vectorizer, String):
+# Predict() predicts the sentiment of String using the Vectorizer and Model passed to it
+# NOTE : Both Vectorizer and Model both should be fitted on some datasets before making a prediction
+def Predict(Vectorizer, Model, String):
     # Create a vector of input String 
     string_vector = Vectorizer.transform([String])
     # Use the model to predict the sentiment of the input string
