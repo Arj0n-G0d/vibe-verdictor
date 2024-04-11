a6b528ca18d33e051ac7738aa51dff660c9c5829
this-dot-god <gaurxarjun@gmail.com>
2024-04-11 18:37:13 +0530

    FIX - TrainModel() taking a lot of time to train the model Primary Cause - I think my custom SpaCy tokenizer (SpacyTokenizer() in model/Tokenizer.py) was causing some problems So removed both model/Tokenizer.py and model/Vectorizer.py 1) TrainModel() now creates TfidfVectorizer object itself with the default sklearn tokenizer (Not the custom one) 2) TrainModel() now returns both the fitted classifier model as well as the fitted vectorizer 3) SaveModel() and LoadModel() now work with file - "model.pkl" instead of "Model.pkl" 4) Predict() now requires Vectorizeras well as Model object (both fitted) to be passed as parameters. It uses the passed Vectorizer to vectorize the passed String and then uses the vectorized String to return the prediction made by the passed Model

--- model/ModelUtilities.py
+++ model/ModelUtilities.py
@@ -1,7 +1,7 @@
 # Import the PreprocessDatasets function from preprocessing/Preprocess.py 
 from preprocessing.Preprocess import PreprocessDatasets  
-# Import the TFIDFVectorizer function from model/Vectorizer.py 
-from model.Vectorizer import TFIDFVectorizer  
+# Import TfidfVectorizer function from sklear.feature_extraction.text module for creating a text vectorizer object
+from sklearn.feature_extraction.text import TfidfVectorizer 
 # Import the Support Vector Machine classifier from sklearn
 from sklearn import svm  
 # Import the pickle module for saving and loading models
@@ -15,34 +15,37 @@ def TrainModel(DatasetPath):
     dataset = PreprocessDatasets(DatasetPath)
     # Extract the 'text' column from the dataset
     text = dataset.pop("text")
-    # Create an instance of the TFIDFVectorizer
-    vectorizer = TFIDFVectorizer()
+    # Create an instance of the TfidfVectorizer
+    vectorizer = TfidfVectorizer()
     # Fit and transform the text data using the vectorizer
     vectorized_text = vectorizer.fit_transform(text)
     # Extract the 'sentiment' column from the dataset
     sentiment = dataset.pop("sentiment")
     # Train the classifier using the vectorized text data and sentiment labels
     classifier.fit(vectorized_text, sentiment)
-    # Return the trained classifier
-    return classifier
+    # Return the fitted Model as well as the fitted Vectorizer
+    return vectorizer, classifier
 
 # SaveModel() makes pickle of the Model passed to it
 def SaveModel(Model):
-    # Open a file named "Model.pkl" in binary write mode
-    with open("Model.pkl", "wb") as file:
+    # Open a file named "model.pkl" in binary write mode
+    with open("model.pkl", "wb") as file:
         # Serialize and save the model to the file
         pickle.dump(Model, file)
 
-# LoadModel() loads the model present in file "Model.pkl" and returns it
+# LoadModel() loads the model present in file "model.pkl" and returns it
 def LoadModel():
-    # Open the file "Model.pkl" in binary read mode
-    with open("Model.pkl", "rb") as file:
+    # Open the file "model.pkl" in binary read mode
+    with open("model.pkl", "rb") as file:
         # Deserialize and load the model from the file
         model = pickle.load(file)
     # Return the loaded model
     return model
 
-# Predict() predicts the sentiment of String using the Model passed to it
-def Predict(Model, String):
+# Predict() predicts the sentiment of String using the Model and Vectorizer passed to it
+# NOTE : Both Model and Vectorizer both should be fitted on some datasets before making a prediction
+def Predict(Model, Vectorizer, String):
+    # Create a vector of input String 
+    string_vector = Vectorizer.transform([String])
     # Use the model to predict the sentiment of the input string
-    return Model.predict([String])[0]
+    return Model.predict(string_vector)[0]
