ec6a10b01ad7210e2be4f7b3a5c6f5aab45e75a7
this-dot-god <gaurxarjun@gmail.com>
2024-04-13 00:32:32 +0530

    MODIFY - app.py 1) Added some comments in app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+# Import the Streamlit library
 import streamlit as st
+# Import the Model class from model/Model.py
 from model.Model import Model
-column1, column2 = st.columns([1,9])
-column1.image("content/Pasted image 1.png")
-column2.markdown("<h2 style = 'text-align:left; color:black;'>Vibe Verdictor</h2>",unsafe_allow_html=True)
-text = st.text_input("This app will make judgements about your vibes!! :)")
-if(text!="") :
+
+# Create two columns in the app layout, with a ratio of 1:9
+column1, column2 = st.columns([1, 9])
+# Display an image in the first column
+column1.image("content/logo.png")
+# Display a heading in the second column
+column2.markdown("<h2 style='text-align:left; color:black;'>Vibe Verdictor</h2>", unsafe_allow_html=True)
+
+# Create a text input box for user input
+text = st.text_input("This app will make judgments about your vibes!! :)")
+
+# Check if the text input is not empty
+if text != "" :
+    # Create an instance of the Model class
     model = Model()
+    # Use the model to predict the vibes based on the user input
     prediction = model.Predict(text)
+    # Display the prediction
     st.markdown(f"You have been verdicted of having {prediction} vibes!!")
+
+# Display a footer message
 st.markdown("Made with :heart: by this_dot_god")
